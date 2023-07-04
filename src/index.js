baf8586ab6118563bb0d122e33d2682cf22edd7b
Victoriia-Ripka <vikaripka@gmail.com>
2023-07-04 13:28:18 +0200

    added netlify-cli package

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import "./fonts/Lora/Lora-Italic-VariableFont_wght.ttf";
-import "./fonts/Lora/Lora-VariableFont_wght.ttf";
-import "modern-normalize";
+import './fonts/Lora/Lora-Italic-VariableFont_wght.ttf';
+import './fonts/Lora/Lora-VariableFont_wght.ttf';
+import 'modern-normalize';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/svitlana-ripka">
+      <BrowserRouter basename="/svitlana-ripka.netlify.app">
         <App />
       </BrowserRouter>
     </ThemeProvider>
